0a48da7c2b9c0fdc0f600578bcfc9c74930da366
xy5873@rit.edu <xy5873@g.rit.edu>
2021-12-17 21:39:21 -0500

    initial commit

---
+++ src/GUI/gui.java
@@ -0,0 +1,14 @@
+package GUI;
+
+import javafx.application.Application;
+
+public class gui {
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: java HoppersPTUI filename");
+        } else {
+            Application.launch(args);
+        }
+    }
+}
---
+++ src/PTUI/ptui.java
@@ -0,0 +1,14 @@
+package PTUI;
+
+import javafx.application.Application;
+
+public class ptui {
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: java HoppersPTUI filename");
+        } else {
+            Application.launch(args);
+        }
+    }
+}
